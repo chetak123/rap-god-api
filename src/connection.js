ddaf9d8f4c443ab80c8d906812915e642f44608f
Ayushman Mishra <ayushvidushi01@gmail.com>
2022-04-08 09:25:39 +0530

    added schema in both

--- src/connection.js
+++ src/connection.js
@@ -1,12 +1,15 @@
-import { setDirectory, consistencies } from "express-cassandra"
+import { setDirectory, consistencies, uuid } from "express-cassandra"
+var ExpressCassandra = require('express-cassandra');
 
+
+/*
 setDirectory(__dirname + "/models").bind(
   {
     clientOptions: {
       contactPoints: ["127.0.0.1"],
       localDataCenter: "datacenter1",
       protocolOptions: { port: 9042 },
-      keyspace: "CassTest",
+      keyspace: "rapLyrics",
       queryOptions: { consistency: consistencies.one },
       socketOptions: { readTimeout: 0 },
     },
@@ -25,3 +28,56 @@ setDirectory(__dirname + "/models").bind(
     }
   }
 )
+*/
+let models = ExpressCassandra.createClient({
+  clientOptions: {
+      contactPoints: ['127.0.0.1'],
+      localDataCenter: 'datacenter1',
+      protocolOptions: { port: 9042 },
+      keyspace: 'k8ssandra',
+      queryOptions: {consistency: ExpressCassandra.consistencies.one}
+  },
+  ormOptions: {
+      defaultReplicationStrategy : {
+          class: 'SimpleStrategy',
+          replication_factor: 1
+      },
+      migration: 'safe',
+  }
+});
+
+var MyModel = models.loadSchema('cassDb1', {
+  fields:{
+    video_link :     "text",
+    lyrics     :     "text",
+    label      :     "text",
+    //date_of_release :"text",
+    singer	   :     "text",
+    miscell_inf :	 "text",
+    lyricist_name :	 "text", // creator of rap
+    rap_id :         "uuid", // 
+  },
+  key:[["rap_id"],"lyricist_name"]
+});
+
+MyModel.syncDB(function(err, result) {
+  if (err) throw err;
+ // creates migrations and syncs it with the DB
+});
+var instance = new models.instance.cassDb1({
+  video_link :     "video1",
+        lyrics     :     "no_ly",
+        label      :     "testlabel",
+        //date_of_release :"mausi",
+        singer	   :     "ayush",
+        miscell_inf :	   "not",
+        lyricist_name :	 "hema", // creator of rap
+        rap_id :         uuid(), // 
+});
+instance.save(function(err){
+  if(err) {
+      console.log(err);
+      return;
+  }
+  console.log('Yuppiie!');
+});
